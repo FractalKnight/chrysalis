046d4ad738a1eafeed3d031e9e18ddbf212c5b6a
fract4l <fract4l>
2023-08-09 12:11:26 -0500

    uploading README LICENSE and starter script

---
+++ README.md
@@ -0,0 +1,26 @@
+# Chrysalis Backup
+
+This package is used to create an automated backup. 
+
+## Getting Started
+
+### Dependencies
+
+* MacOS Ventura or newer
+* go version go1.20.2 darwin/amd64
+
+### Installing
+
+* Download the zip or run ```git clone https://github.com/FractalKnight/chrysalis.git```
+
+### Executing program
+
+* Run ```./build_chrysalis.sh```
+
+
+## License
+
+This project is licensed under the MIT License - see the LICENSE.md file for details
+
+## Acknowledgments
+OUR PBS FAMILY AND YOU!
---
+++ build_chrysalis.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+
+function ProgressBar {
+# Process data
+	let _progress=(${1}*100/${2}*100)/100
+	let _done=(${_progress}*4)/10
+	let _left=40-$_done
+# Build progressbar string lengths
+	_done=$(printf "%${_done}s")
+	_left=$(printf "%${_left}s")
+
+# 1.2 Build progressbar strings and print the ProgressBar line
+# 1.2.1 Output example:
+# 1.2.1.1 Progress : [########################################] 100%
+printf "\rProgress : [${_done// /#}${_left// /-}] ${_progress}%%"
+
+}
+
+_start=1
+_end=300
+
+printf "Starting the Chrysalis process...\n"
+sleep 10
+nohup go run chrysalis/chrysalis.go & 
+printf "Finding files for backup\n"
+sleep 30
+printf "Adding files to Chrysalis\n"
+sleep 5
+for number in $(seq ${_start} ${_end})
+do 
+	sleep 0.1
+	ProgressBar ${number} ${_end}
+done
+printf "\nDone creating Chrysalis"
